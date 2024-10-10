93986d256469d1393b1b3a7f6a6d54aca15ee835
kharu_2.17 <kharu217.dev@gmail.com>
2024-10-10 19:42:28 +0900

    photo size problem

--- SRCNN(improve)/PSNR_SSIM.py
+++ SRCNN(improve)/PSNR_SSIM.py
@@ -1,3 +1,4 @@
+#%%
 import matplotlib.figure
 import matplotlib.pyplot
 from model import SRCNN
@@ -7,19 +8,19 @@ import skimage
 import cv2
 import numpy as np
 
-device = torch.device('cuda')
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 model_f = SRCNN()
-model_f.load_state_dict(torch.load(r"models\face_m.h5"))
+model_f.load_state_dict(torch.load(r'SRCNN(improve)\models\face_m.h5', map_location=device))
 
 model_n = SRCNN()
-model_n.load_state_dict(torch.load(r"models\normal.h5"))
+model_n.load_state_dict(torch.load(r'SRCNN(improve)\models\normal.h5', map_location=device))
 
 test_img = "testing.jpg"
 
 temp_img_r = cv2.imread(test_img)
-temp_img_r = cv2.cvtColor(temp_img_r, cv2.COLOR_BGR2RGB)
-input_img_r = temp_img_r.astype(np.float32)/255.0
+cvtemp = cv2.cvtColor(temp_img_r, cv2.COLOR_BGR2RGB)
+input_img_r = cvtemp.astype(np.float32)/255.0
 input_img_r = input_img_r.transpose((2, 0, 1))
 input_img_r = torch.tensor(input_img_r).unsqueeze(0)
 
@@ -29,19 +30,27 @@ with torch.no_grad() :
 
 f_model_img = f_model_img.squeeze().cpu().numpy().transpose((1,2,0))
 n_model_img = n_model_img.squeeze().cpu().numpy().transpose((1,2,0))
+compare_img = cv2.imread(test_img)
+#temp_img_r = temp_img_r.squeeze()
+
+print(type(compare_img))
+print(type(f_model_img))
+print(type(n_model_img))
 
 fig = matplotlib.pyplot.figure()
 
-print(f_model_img.shape())
+print(f_model_img.shape)
+print(n_model_img.shape)
+print(compare_img.shape)
 
-psnr_f_o = skimage.metrics.peak_signal_noise_ratio(temp_img_r, f_model_img)
-ssim_f_o = skimage.metrics.structural_similarity(temp_img_r, f_model_img)
+psnr_f_o = skimage.metrics.peak_signal_noise_ratio(compare_img, f_model_img)
+ssim_f_o = skimage.metrics.structural_similarity(compare_img, f_model_img, channel_axis=2)
 
-psnr_n_o = skimage.metrics.peak_signal_noise_ratio(temp_img_r, n_model_img)
-ssim_n_o = skimage.metrics.structural_similarity(temp_img_r, n_model_img)
+psnr_n_o = skimage.metrics.peak_signal_noise_ratio(compare_img, n_model_img)
+ssim_n_o = skimage.metrics.structural_similarity(compare_img, n_model_img, channel_axis=2)
 
-psnr_o = skimage.metrics.peak_signal_noise_ratio(temp_img_r, temp_img_r)
-ssim_o = skimage.metrics.structural_similarity(temp_img_r, temp_img_r)
+psnr_o = skimage.metrics.peak_signal_noise_ratio(compare_img, compare_img)
+ssim_o = skimage.metrics.structural_similarity(compare_img, compare_img, channel_axis=2)
 
 ax1 = fig.add_subplot(1, 3, 1)
 ax1.imshow(f_model_img)
@@ -52,7 +61,7 @@ ax2.imshow(n_model_img)
 ax2.set_xlabel(f'psnr : {psnr_n_o}\n ssim : {ssim_n_o}')
 
 ax3 = fig.add_subplot(1, 3, 3)
-ax3.imshow(temp_img_r)
+ax3.imshow(compare_img)
 ax2.set_xlabel(f'psnr : {psnr_o}\n ssim : {ssim_o}')
 
 matplotlib.pyplot.show()
